bcdbaad9bf9fd966542d9620db6c6fc21839cd55
ESNAY Roland (SNCF VOYAGEURS / Direction de Lignes TER Est Provence Alpes / SIEGE UO RC TOULON) <7807212a@commun.ad.sncf.fr>
2023-01-24 12:35:41 +0100

    Express 04 finished

--- userHandlers.js
+++ userHandlers.js
@@ -1,22 +1,44 @@
 const postUser = (req, res) => {
-const { firstname, lastname, email, city, language } = req.body;
+  const { id, firstname, lastname, email, city, language } = req.body;
 
-database
-.query(
-"INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-[firstname, lastname, email, city, language]
-)
-.then(([result]) => {
-res.location(`/api/users/${result.insertId}`).sendStatus(201);
-})
-.catch((err) => {
-console.error(err);
-res.status(500).send("Error saving the users");
-});
+  database
+    .query(
+      "INSERT INTO users(id, firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [id, firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the users");
+    });
 };
 
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
+};
 const users = [
   {
+    id: 1,
     firstname: "John",
     lastname: "Doe",
     email: "john.doe@example.com",
@@ -24,6 +46,7 @@ const users = [
     language: "English",
   },
   {
+    id: 2,
     firstname: "Appius",
     lastname: "valeriy.appius@example.com",
     email: "Moscow",
@@ -31,6 +54,7 @@ const users = [
     language: "Valeriy",
   },
   {
+    id: 3,
     firstname: "Ralf",
     lastname: "Geronimo",
     email: "ralf.geronimo@example.com",
@@ -38,6 +62,7 @@ const users = [
     language: "Italian",
   },
   {
+    id: 4,
     firstname: "Maria",
     lastname: "Iskandar",
     email: "maria.iskandar@example.com",
@@ -45,6 +70,7 @@ const users = [
     language: "German",
   },
   {
+    id: 5,
     firstname: "Jane",
     lastname: "Doe",
     email: "jane.doe@example.com",
@@ -52,6 +78,7 @@ const users = [
     language: "English",
   },
   {
+    id: 6,
     firstname: "Johanna",
     lastname: "Martino",
     email: "johanna.martino@example.com",
@@ -88,4 +115,5 @@ module.exports = {
   getUsers,
   getUserById,
   postUser,
+  updateUser,
 };
