65b5af497be608869e36a6fee8bc64960f70568b
ESNAY Roland (SNCF VOYAGEURS / Direction de Lignes TER Est Provence Alpes / SIEGE UO RC TOULON) <7807212a@commun.ad.sncf.fr>
2023-01-24 11:44:48 +0100

    Fin quetes 3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -13,13 +14,17 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
-const userHandlers = require ("./userHandlers")
+const userHandlers = require("./userHandlers");
 
-
+// GET
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+
+//POST
+app.post("/api/movies", movieHandlers.postMovie);
+app.post ("/api/users",userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -1,74 +1,91 @@
+const postUser = (req, res) => {
+const { firstname, lastname, email, city, language } = req.body;
+
+database
+.query(
+"INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+[firstname, lastname, email, city, language]
+)
+.then(([result]) => {
+res.location(`/api/users/${result.insertId}`).sendStatus(201);
+})
+.catch((err) => {
+console.error(err);
+res.status(500).send("Error saving the users");
+});
+};
+
 const users = [
   {
-    firstname:'John',
-    lastname:'Doe',
-    email:'john.doe@example.com',
-    city:'Paris',
-    language:'English'
+    firstname: "John",
+    lastname: "Doe",
+    email: "john.doe@example.com",
+    city: "Paris",
+    language: "English",
   },
   {
-    firstname:'Appius',
-    lastname:'valeriy.appius@example.com',
-    email:'Moscow',
-    city:'Russian',
-    language:'Valeriy',
+    firstname: "Appius",
+    lastname: "valeriy.appius@example.com",
+    email: "Moscow",
+    city: "Russian",
+    language: "Valeriy",
   },
   {
-    firstname:'Ralf',
-    lastname:'Geronimo',
-    email:'ralf.geronimo@example.com',
-    city:'New York',
-    language:'Italian'
+    firstname: "Ralf",
+    lastname: "Geronimo",
+    email: "ralf.geronimo@example.com",
+    city: "New York",
+    language: "Italian",
   },
   {
-    firstname:'Maria',
-    lastname:'Iskandar',
-    email:'maria.iskandar@example.com',
-    city:'New York',
-    language:'German'
+    firstname: "Maria",
+    lastname: "Iskandar",
+    email: "maria.iskandar@example.com",
+    city: "New York",
+    language: "German",
   },
   {
-    firstname:'Jane',
-    lastname:'Doe',
-    email:'jane.doe@example.com',
-    city:'London',
-    language:'English'
+    firstname: "Jane",
+    lastname: "Doe",
+    email: "jane.doe@example.com",
+    city: "London",
+    language: "English",
   },
   {
-    firstname:'Johanna',
-    lastname:'Martino',
-    email:'johanna.martino@example.com',
-    city:'Milan',
-    language:'Spanish'
+    firstname: "Johanna",
+    lastname: "Martino",
+    email: "johanna.martino@example.com",
+    city: "Milan",
+    language: "Spanish",
   },
 ];
 const database = require("./database");
 
 const getUsers = (req, res) => {
-  database
-  res.status(200).json(users)
-
+  database;
+  res.status(200).json(users);
 };
 
 const getUserById = (req, res) => {
   const userId = parseInt(req.params.id);
 
   database
-  .query("select * from users where id = ?", [userId])
-  .then(([users]) => {
-    if (users[0] != null) {
-      res.json(users[0]);
-    } else {
-      res.status(404).send("Not Found");
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error retrieving data from database");
-  });
+    .query("select * from users where id = ?", [userId])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 module.exports = {
   getUsers,
-  getUserById
-}
+  getUserById,
+  postUser,
+};
